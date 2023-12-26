04a01bba6f405f013dfbcab54014a2d8ae620e07
tanyelibeyza <148748793+tanyelibeyza@users.noreply.github.com>
2023-12-26 13:55:16 +0300

    Add files via upload

---
+++ r.g.b renk değişim/r.g.b renk değişim/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace r.g.b_renk_değişim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
